f204dc6473e437e065ba5585251d5ce36ec45ec8
p4n1k0 <gabre.tarick@gmail.com>
2023-03-13 20:01:47 -0300

    recebe conteudo HTML da pag d uma noticia c seus devidos conteudos

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -18,17 +18,33 @@ def fetch(url):
 # Requisito 2
 def scrape_updates(html_content):
     return parsel.Selector(html_content).css(
-        '.cs-overlay-link::attr(href)').getall()
+       '.cs-overlay-link::attr(href)').getall()
 
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    return parsel.Selector(html_content).css('.next::attr(href)').get()
+    response = parsel.Selector(html_content).css('.next::attr(href)').get()
+    if not response:
+        return None
+    return response
 
 
 # Requisito 4
 def scrape_news(html_content):
-    """Seu código deve vir aqui"""
+    return {
+        'url': parsel.Selector(html_content).css(
+            '.pk-share-buttons-wrap').attrib['data-share-url'],
+        'title': parsel.Selector(html_content).css(
+            '.entry-title::text').get().strip('\xa0'),
+        'timestamp': parsel.Selector(html_content).css(
+            '.meta-date::text').get(),
+        'writer': parsel.Selector(html_content).css('.n::text').get(),
+        'reading_time': int(parsel.Selector(html_content).css(
+            '.meta-reading-time::text').get().split(' ')[0]),
+        'summary': parsel.Selector(html_content).css(
+            '.entry-content p').xpath('string()').get().strip(),
+        'category': parsel.Selector(html_content).css('.label::text').get()
+    }
 
 
 # Requisito 5
