5fa871230fa3d134e533cf7ebf0dce2eaee888a2
p4n1k0 <gabre.tarick@gmail.com>
2023-03-17 21:18:40 -0300

    Busca de notícias do banco de dados por data formato ISO

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,15 +1,21 @@
 from tech_news.database import search_news
+from datetime import datetime
 
 
 # Requisito 7
 def search_by_title(title):
-    return [(new["title"], new["url"]) for new in search_news(
-        {"title": {"$regex": title, "$options": "i"}})]
+    return [(new['title'], new['url']) for new in search_news(
+        {'title': {'$regex': title, '$options': 'i'}})]
 
 
 # Requisito 8
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        date = datetime.fromisoformat(date).strftime('%d/%m/%Y')
+        return [(new['title'], new['url'])
+                for new in search_news({'timestamp': date})]
+    except ValueError:
+        raise ValueError('Data inválida')
 
 
 # Requisito 9
